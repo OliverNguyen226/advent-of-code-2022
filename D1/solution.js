1189ba98122feb4ae60c73c619b5a8bc4c747cd5
Hoang Cuong Nguyen <hcuong.ng.ca@gmail.com>
2022-12-02 14:40:19 -0800

    day 1 part 2

--- D1/solution.js
+++ D1/solution.js
@@ -6,8 +6,20 @@ function findMax(input) {
   return input.reduce((p, c) => (Number(p) > Number(c) ? p : c));
 }
 
+function findMaxes(input, num = 1) {
+  const maxes = input.splice(0, num).sort();
+  input.forEach((e) => {
+    if (e > maxes[maxes.length - 1]) {
+      maxes.shift();
+      maxes.push(e);
+    }
+  })
+  return addCalc(maxes);
+}
+
 function findWeight(elves) {
-  return findMax(elves.map(addCalc));
+  // return findMax(elves.map(addCalc));
+  return findMaxes(elves.map(addCalc), 3);
 }
 
 module.exports = findWeight;
